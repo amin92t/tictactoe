7e18c1e26c01646584020dcaf915bb566608d5af
Amin Ataei <amin.ataei92@gmail.com>
2023-03-12 14:44:54 +0330

    complete tictactoe mini project

---
+++ app.js
@@ -0,0 +1,78 @@
+const gameStatus = document.querySelector('.game__status-turn')
+const cellsElement = document.querySelectorAll('.cell')
+const resetBtn = document.querySelector('.game__button-btn')
+
+let player = 'O'
+let cells = []
+let playing = true
+gameStatus.innerText = `${player}`
+
+cellsElement.forEach((item, index) => {
+    cells[index] = {
+        element: item,
+        value: null,
+        reset() {
+            this.element.classList.remove(this.value)
+            this.value = null
+        }
+    }
+    item.addEventListener('click', (e) => { cellHandler(e, index) })
+})
+
+function cellHandler(event, index) {
+
+    if (playing && cells[index].value == null) {
+        cells[index].value = player
+        cells[index].element.classList.add(player)
+        togglePlayer()
+        checkWinner()
+    }
+
+}
+
+function togglePlayer() {
+    player = player == 'O' ? 'X' : 'O'
+    gameStatus.innerText = `${player}`
+}
+
+function checkWinner() {
+    let winner = null
+    if (cells[0].value !== null && cells[0].value == cells[1].value
+        && cells[0].value == cells[2].value) winner = cells[0].value
+    else if (cells[3].value !== null && cells[3].value == cells[4].value
+        && cells[3].value == cells[5].value) winner = cells[3].value
+    else if (cells[6].value !== null && cells[6].value == cells[7].value
+        && cells[6].value == cells[8].value) winner = cells[6].value
+    else if (cells[0].value !== null && cells[0].value == cells[6].value
+        && cells[0].value == cells[6].value) winner = cells[0].value
+    else if (cells[1].value !== null && cells[1].value == cells[4].value
+        && cells[1].value == cells[7].value) winner = cells[1].value
+    else if (cells[2].value !== null && cells[2].value == cells[5].value
+        && cells[2].value == cells[8].value) winner = cells[2].value
+    else if (cells[0].value !== null && cells[0].value == cells[4].value
+        && cells[0].value == cells[8].value) winner = cells[0].value
+    else if (cells[2].value !== null && cells[2].value == cells[4].value
+        && cells[2].value == cells[6].value) winner = cells[2].value
+    if (winner != null) {
+        playing = false
+        gameStatus.innerText = `Game End - Winner : ${player}`
+        resetBtn.classList.add('show')
+        resetBtn.addEventListener('click', restart)
+    } else if (cells[0].value !== null && cells[1].value !== null
+        && cells[2].value !== null && cells[3].value !== null && cells[4].value !== null
+        && cells[5].value !== null && cells[6].value !== null && cells[7].value !== null
+        && cells[8].value !== null
+    ) {
+        playing = false
+        gameStatus.innerText = `Game End - without Winner`
+        resetBtn.classList.add('show')
+        resetBtn.addEventListener('click', restart)
+    }
+}
+
+function restart() {
+    playing = true
+    player = player == 'O' ? 'X' : 'O'
+    gameStatus.innerText = `${player}`
+    cells.forEach(item => item.reset())
+}
